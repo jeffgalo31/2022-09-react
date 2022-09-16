ddcb4ab15047fceb12b7cc3153450138ac0922ae
Rod Nolan <rnolan@aquent.com>
2022-09-16 17:01:02 -0400

    controlled and uncontrolled forms
    up to the end of the day on Friday

--- src/Child.js
+++ src/Child.js
@@ -2,20 +2,46 @@ import React from "react";
 
 export class Child extends React.Component {
 
-  // constructor(props) {
-  //   super(props)
-  // }
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.fnRef = React.createRef();
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    // const firstNameValueFromFormField = document.getElementById('fn').value;
+    const firstNameValueFromFormField = this.fnRef.current.value;
+    this.props.p1UpdaterFunction(firstNameValueFromFormField);
+  }
+
+  handleChange(event) {
+    console.log('the form field was changed')
+    console.log(event.target.value);
+    this.props.p1UpdaterFunction(event.target.value);
+  }
 
   render() {
     return <>
       <h2>Child</h2>
-      <p>
-        {this.props.p1}
-      </p>
+      <p>{this.props.p1}</p>
       <button onClick={() => {
         this.props.p1UpdaterFunction('sent from child component');
-        // console.log(somePropValue);
       }}>this WILL work - called from child</button>
+
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="fn">First Name</label>
+        <input 
+          id="fn" 
+          type="text" 
+          placeholder={this.props.p1} 
+          
+          value={this.props.p1}
+          onChange={this.handleChange} 
+          ref={this.fnRef}
+          />
+        <input type="submit" />
+      </form>
 
     </>
   }
