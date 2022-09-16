40912084efaea94e1ba74278422b9ac2ea4cf451
Rod Nolan <rnolan@aquent.com>
2022-09-16 17:15:30 -0400

    challenge solution 1
    updating complex data, not just primitives

--- src/Child.js
+++ src/Child.js
@@ -7,26 +7,37 @@ export class Child extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.fnRef = React.createRef();
+    this.lnRef = React.createRef();
   }
   handleSubmit(event) {
     event.preventDefault();
-    // const firstNameValueFromFormField = document.getElementById('fn').value;
     const firstNameValueFromFormField = this.fnRef.current.value;
-    this.props.p1UpdaterFunction(firstNameValueFromFormField);
+    const lastNameValueFromFormField = this.lnRef.current.value;
+    this.props.personUpdaterFunction({
+      fn: firstNameValueFromFormField,
+      ln: lastNameValueFromFormField
+    });
   }
 
   handleChange(event) {
     console.log('the form field was changed')
-    console.log(event.target.value);
-    this.props.p1UpdaterFunction(event.target.value);
+    console.log(event.target.id, event.target.value);
+    this.props.personUpdaterFunction({
+      ...this.props.person, ...{[event.target.id]: event.target.value}
+    });
+
+    // note the use of the [] on the LEFT side of the colon character in the object defined above
+    // this allows me to reference a variable as the property's NAME 
+    // this technique lets me reuse this handleChange function for both fields
+    // because I don't have to hard code {fn: event.target.value} or {ln: event.target.value}
   }
 
   render() {
     return <>
       <h2>Child</h2>
-      <p>{this.props.p1}</p>
+      <p>{this.props.person.fn} {this.props.person.ln}</p>
       <button onClick={() => {
-        this.props.p1UpdaterFunction('sent from child component');
+        this.props.personUpdaterFunction({...this.props.person, ln: 'Barker'});
       }}>this WILL work - called from child</button>
 
       <form onSubmit={this.handleSubmit}>
@@ -34,11 +45,19 @@ export class Child extends React.Component {
         <input 
           id="fn" 
           type="text" 
-          placeholder={this.props.p1} 
-          
-          value={this.props.p1}
+          placeholder={this.props.person.fn} 
+          value={this.props.person.fn}
           onChange={this.handleChange} 
           ref={this.fnRef}
+          />
+        <label htmlFor="ln">Last Name</label>
+        <input 
+          id="ln" 
+          type="text" 
+          placeholder={this.props.person.ln} 
+          value={this.props.person.ln}
+          onChange={this.handleChange} 
+          ref={this.lnRef}
           />
         <input type="submit" />
       </form>
--- src/Parent.js
+++ src/Parent.js
@@ -2,26 +2,19 @@ import { useState } from "react";
 import { Child } from "./Child"
 
 export const Parent = () => {
-  
-  // let somePropValue = 'sdhkfghdsfjkl';
-  const [somePropValue, setSomePropValue] = useState('sdhkfghdsfjkl');
-  
+  const [person, setPerson] = useState({
+    fn: 'Mickey',
+    ln: 'Mouse'
+  });
+
   return <>
-  <h1>Parent</h1>
+    <h1>Parent</h1>
 
-  {/* 
-  this is an example of what NOT to do
-  <button onClick={() => {
-    somePropValue = 'x,kcbvkjdhsbvkjhsbjkvhcbjvbxc';
-    console.log(somePropValue);
-  }}>this will not work</button> */}
+    <button onClick={() => {
+      setPerson({...person, fn: 'Bob'});
+    }}>this WILL work - called from parent</button>
 
-  <button onClick={() => {
-    setSomePropValue('x,kcbvkjdhsbvkjhsbjkvhcbjvbxc');
-    // console.log(somePropValue);
-  }}>this WILL work - called from parent</button>
-
-  <Child p1={somePropValue} p1UpdaterFunction={setSomePropValue} />
+    <Child person={person} personUpdaterFunction={setPerson} />
   
   </>
 }
