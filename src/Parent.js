a56a1138afa459099e700883609c2d1117d56c01
Rod Nolan <rnolan@aquent.com>
2022-09-19 13:16:02 -0400

    end of morning session on Monday #2

--- src/Parent.js
+++ src/Parent.js
@@ -1,39 +1,63 @@
 import { useState } from "react";
-import { Child } from "./Child"
+// import { Child } from "./Child"
+import { ChildAddForm } from "./ChildAddForm";
+import { v4 as uuidv4 } from 'uuid';
 
 export const Parent = () => {
 
-  let someLocalVar = 'sdfhfshbfsdgfd'; // doesn't trigger a re-render
-  const [person, setPerson] = useState({
-    fn: 'Mickey',
-    ln: 'Mouse'
-  });
-  // state changes DO trigger a re-render
+  const [people, setPeople] = useState([
+    {
+      id: 123,
+      fn: 'Mickey',
+      ln: 'Mouse'
+    },
+    {
+      id: 543,
+      fn: 'Bill',
+      ln: 'Gates'
+    },
+  ]);
 
-  const buttonClickHandler = (event) => {
-    // this WILL trigger a re-render
-    setPerson({ ...person, fn: event.target.innerText });
+  const addPerson = (person) => {
+    let peopleCopy = [...people]; // shallow copy Object.assign AND ...spread operator
+
+    peopleCopy.push({ ...person, id: uuidv4() });
+    setPeople(peopleCopy);
   }
 
   return <>
     <h1>Parent</h1>
-    <p>{person.fn} {person.ln}</p>
+    <ol>
+      {
+        people.map(
+          (person) => {
+            return <li key={person.id}>{person.fn} {person.ln} </li>
+          }
+        )
+      }
+    </ol>
 
-
-    {someLocalVar}<br />
     <button onClick={() => {
-      // this will NOT trigger a re-render
-      someLocalVar = '5e5641e 62igfjgfjkhsdzg';
-      console.log(someLocalVar);
-    }}>update a local var</button>
+      // people[1].fn = "Jack";
+      // console.log(people[1]);
 
-    <button onClick={ buttonClickHandler }>Bob</button>
-    <button onClick={ buttonClickHandler }>Bill</button>
+      let peopleCopy = [...people]; // shallow copy Object.assign AND ...spread operator
 
-    <Child
+      const newBill = { ...peopleCopy[1] };
+      newBill.fn = "Bob";
+
+      peopleCopy[1] = newBill;
+
+      setPeople(peopleCopy);
+    }}>
+      update bill
+    </button>
+    <ChildAddForm
+      personAdderFunction={addPerson}
+    />
+    {/* <Child
       person={person}
       personUpdaterFunction={setPerson}
-    />
-
+    /> */}
   </>
 }
